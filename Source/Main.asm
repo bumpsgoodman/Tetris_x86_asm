2d0dcc8651a2508679ac327a6c63f2fea061fc0a
bumpsgoodman <bumpsgoodman@gmail.com>
2025-01-28 06:09:05 +0900

    블럭 색상 추가, 게임 오버 문구 추가, 버그 수정

--- Source/Main.asm
+++ Source/Main.asm
@@ -7,9 +7,8 @@
 ; 4. 레이블은 snake_case입니다.
 ; 5. 레이블은 접두사 "lb_"가 붙습니다.
 ; 6. 모든 함수는 반환 레이블을 넣습니다. 예) lb_main_return:
-; 7. 어셈블러 종속적인 기능은 최대한 사용하지 않습니다.
-; 8. 주석을 최대한 자세하게 작성해야 합니다.
-; 9. 반환값에 주석으로 비트 수가 적혀 있지 않다면 기본적으로 32비트입니다.
+; 7. 주석을 최대한 자세하게 작성해야 합니다.
+; 8. 반환값에 주석으로 비트 수가 적혀 있지 않다면 기본적으로 32비트입니다.
 ;=========================================
 
 title Tetris_x86_assembly
@@ -24,11 +23,15 @@ INCLUDE Game.inc
 EXTRN ExitProcess@4 : PROC
 
 .data
-hello DB 'hello', 0     ; 테스트용
+GAME_OVER_TEXT  DB 'GAME OVER !', 0
+EXIT_KEY_TEXT   DB 'PRESS ENTER KEY...', 0
 
 FRAME_TIME  EQU 33  ; 30 fps
 start_time  DWORD   ?
 end_time    DWORD   ?
+
+VK_ENTER            EQU     00Dh
+enterKeyState       DD   0
 
 .code
 
@@ -37,11 +40,15 @@ end_time    DWORD   ?
 ; 설명: 이 함수는 진입점 함수입니다.
 ;       테트리스 게임 루프를 수행합니다.
 ; 매개변수: 없음
-; 반환값: 없음
-;
-; 함수 프롤로그/에필로그 없음
+; 반환값: 0
 ;=========================================
 main:
+    ; 프롤로그
+    push ebp
+    mov ebp, esp
+    push edx
+    push edi
+
     call InitConsole    ; 게임 시작 전, 콘솔 초기화
     call InitGame       ; 게임 초기화
 
@@ -51,7 +58,7 @@ main:
 lb_game_loop:
     call IsRunningGame  ; 게임 오버인지 확인하기
     test eax, eax
-    jz lb_main_return
+    jz lb_print_game_over
 
     call DrawGame       ; 게임 그리기
 
@@ -74,7 +81,50 @@ lb_wait_next_frame:
     mov [start_time], eax
     jmp lb_game_loop
 
+lb_print_game_over:
+    call DrawGame
+
+    push 0
+    push 0
+    push 255
+    call SetTextColor
+
+    push 20
+    push 7
+    call GotoXY
+
+    lea edx, GAME_OVER_TEXT
+    push 12
+    push edx
+    call Print
+
+    push 21
+    push 7
+    call GotoXY
+
+    lea edx, EXIT_KEY_TEXT
+    push 19
+    push edx
+    call Print
+
+    ; 엔터키 업데이트
+    lea edi, enterKeyState
+    mov eax, VK_ENTER
+    push edi            ; keyState
+    push eax            ; 가상 키 코드
+    call UpdateKey
+
+    cmp enterKeyState, KEY_STATE_DOWN
+    jne lb_print_game_over
+
 lb_main_return:
+    ; 에필로그
+    pop edi
+    pop edx
+    mov esp, ebp
+    pop ebp
+    ret
+
     push 0
     call ExitProcess@4
 END main
